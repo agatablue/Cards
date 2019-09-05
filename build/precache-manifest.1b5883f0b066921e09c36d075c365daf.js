e8115718679c0350c6a79b3d7cdc9d6763bc3d30
agatablue <agatablue@gmail.com>
2019-09-05 14:18:34 +0200

    add build

---
+++ build/precache-manifest.1b5883f0b066921e09c36d075c365daf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7f7975638a4dd56e1e8ae55724d52f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd014e2eeb7d505b7c33",
+    "url": "/static/css/main.a127ffbf.chunk.css"
+  },
+  {
+    "revision": "1cb8e45db1503ac72ef7",
+    "url": "/static/js/2.aea0afa5.chunk.js"
+  },
+  {
+    "revision": "dd014e2eeb7d505b7c33",
+    "url": "/static/js/main.daf2a8fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
